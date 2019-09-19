fb33a2d74e553e9e2cc9edf20af6a309d5e17c7d
Amin Borjian <borjianamin98@outlook.com>
2019-09-19 17:32:37 +0430

    Change main page and controller to look better

--- src/main/java/ir/ac/sbu/pgen/Main.java
+++ src/main/java/ir/ac/sbu/pgen/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(ResourceUtility.getResource("fxml/MainWindow.fxml"));
+        Parent root = FXMLLoader.load(ResourceUtility.getResource("fxml/Main.fxml"));
         primaryStage.setTitle("PGen");
         primaryStage.getIcons().add(new Image(ResourceUtility.getResourceAsStream("assets/Icon.png")));
 
