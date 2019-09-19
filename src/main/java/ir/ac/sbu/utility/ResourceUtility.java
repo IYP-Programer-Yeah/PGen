8b668b2b69fe5a96f52a3bec26f96a3e3619a93c
Amin Borjian <borjianamin98@outlook.com>
2019-09-19 10:45:27 +0430

    Add a utility to load resource from resourse folder of maven

---
+++ src/main/java/ir/ac/sbu/utility/ResourceUtility.java
@@ -0,0 +1,29 @@
+package ir.ac.sbu.utility;
+
+import ir.ac.sbu.exception.ResourceNotFoundException;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class ResourceUtility {
+    private ResourceUtility() {
+    }
+
+    public static URL getResource(String path) {
+        URL resource = Thread.currentThread().getContextClassLoader().getResource(path);
+        if (resource == null) {
+            throw new ResourceNotFoundException("Resource not found: " + path);
+        } else {
+            return resource;
+        }
+    }
+
+    public static InputStream getResourceAsStream(String path) {
+        InputStream resource = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+        if (resource == null) {
+            throw new ResourceNotFoundException("Resource not found: " + path);
+        } else {
+            return resource;
+        }
+    }
+}
